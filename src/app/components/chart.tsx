603265ba58adaecdce418be9526123adee2f1c77
Caleb Kalejaiye <155694747+heycalebszn@users.noreply.github.com>
2025-01-21 20:00:02 +0100

    Update chart.tsx

--- src/app/components/chart.tsx
+++ src/app/components/chart.tsx
@@ -1,7 +1,7 @@
 import {ChartImg} from "../assets"
 import Image from "next/image";
 
-export const Chart = () =>{
+export const Chart = () => {
     return(
     <section className="ml-[40px] mt-[30px]">
         <Image alt="chart" src={ChartImg} className="w-[790px]" />
